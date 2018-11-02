9689257e69635ea79c049f9130ccc1ba1e4ab4b1
Jonatas <jotunes23@gmail.com>
2018-11-02 20:56:51 -0300

    commit github pages

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        watch: {
+            css: {
+                files: 'css/scss/*.scss',
+                tasks: ['sass']
+            }
+        },
+
+        sass: {                              
+            dist: {                            
+                options: {    
+                    style: 'compressed'                   
+                    // style: 'expanded'
+                },
+                files: {                         
+                    'css/style.css':'css/scss/style.scss'
+                }
+            }
+        }
+
+    });
+  
+    // Carregando os plugins
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+  
+    // Tarefa Default
+    grunt.registerTask('default', ['sass', 'watch']);
+  
+  };
